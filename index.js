734ef3f43fcbba64d5095c5fb73a1062aeb11e61
Mike <110837765+JMikeRivera@users.noreply.github.com>
2024-09-15 03:15:32 -0600

    front

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ const PORT = 3000;
 // Servir archivos estáticos (HTML, CSS, JS) desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
+<<<<<<< Updated upstream
 // Ruta para servir el index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+=======
+// Configuración de la conexión a la base de datos PostgreSQL
+const pool = new Pool({
+  user: 'postgres',      // Reemplaza con tu nombre de usuario de PostgreSQL
+  host: 'localhost',       // La dirección del servidor (localhost si está en tu máquina)
+  database: 'BanTec',      // El nombre de la base de datos que creaste en PostgreSQL
+  password: 'admin',  // La contraseña del usuario
+  port: 5432,              // El puerto por defecto de PostgreSQL
+>>>>>>> Stashed changes
 });
 
 // Escuchar en el puerto definido
